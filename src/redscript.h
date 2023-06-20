f88c80967788c8664150f6270d9fd239046df16d
Jack Humbert <jack.humb@gmail.com>
2023-06-20 19:42:05 -0400

    add extern c

--- src/redscript.h
+++ src/redscript.h
@@ -3,13 +3,13 @@
 struct Decompilation;
 struct ScriptBundle;
 
-struct ScriptBundle* bundle_load(const char* path);
-void bundle_free(struct ScriptBundle* bundle);
+extern "C" struct ScriptBundle* bundle_load(const char* path);
+extern "C" void bundle_free(struct ScriptBundle* bundle);
 
-struct Decompilation* decompile_global(struct ScriptBundle* bundle, const char* name);
-struct Decompilation* decompile_method(struct ScriptBundle* bundle, const char* parent, const char* name);
+extern "C" struct Decompilation* decompile_global(struct ScriptBundle* bundle, const char* name);
+extern "C" struct Decompilation* decompile_method(struct ScriptBundle* bundle, const char* parent, const char* name);
 
-const char* decompilation_code(const struct Decompilation* decompilation);
-const uint32_t* decompilation_line_mapping(const struct Decompilation* decompilation);
-uint32_t decompilation_line_count(const struct Decompilation* decompilation);
-void decompilation_free(struct Decompilation* bundle);
+extern "C" const char* decompilation_code(const struct Decompilation* decompilation);
+extern "C" const uint32_t* decompilation_line_mapping(const struct Decompilation* decompilation);
+extern "C" uint32_t decompilation_line_count(const struct Decompilation* decompilation);
+extern "C" void decompilation_free(struct Decompilation* bundle);
